7e555f74a0aac5c1a0f8054a5e4020066b3295f6
Zenderous314 <163520322+Zenderous314@users.noreply.github.com>
2025-09-12 09:03:16 +0100

    feat:advanced merging Concatenating.py

---
+++ Associate Data Scientist/Joinind Data With Pandas/Advanced Merging and Concatenating.py
@@ -0,0 +1,101 @@
+# Merge employees and top_cust
+empl_cust = employees.merge(top_cust, on='srid', 
+                            how='left', indicator=True)
+# Select the srid column where _merge is left_only
+srid_list = empl_cust.loc[empl_cust['_merge'] == 'left_only', 'srid']
+
+# Get employees not working with top customers
+print(employees[employees['srid'].isin(srid_list)])
+# Merge the non_mus_tcks and top_invoices tables on tid
+tracks_invoices = non_mus_tcks.merge(top_invoices, on='tid')
+
+# Use .isin() to subset non_mus_tcks to rows with tid in tracks_invoices
+top_tracks = non_mus_tcks[non_mus_tcks['tid'].isin(tracks_invoices['tid'])]
+
+# Group the top_tracks by gid and count the tid rows
+cnt_by_gid = top_tracks.groupby(['gid'], as_index=False).agg({'tid':"count"})
+
+# Merge the genres table to cnt_by_gid on gid and print
+print(cnt_by_gid.merge(genres, on='gid'))
+
+# Concatenate the tracks
+tracks_from_albums = pd.concat([tracks_master, tracks_ride, tracks_st],
+                               sort=True)
+print(tracks_from_albums)
+# Concatenate the tracks so the index goes from 0 to n-1
+tracks_from_albums = pd.concat([tracks_master, tracks_ride, tracks_st],
+                               ignore_index=True,
+                               sort=True)
+print(tracks_from_albums)
+
+# Concatenate the tracks, show only columns names that are in all tables
+tracks_from_albums = pd.concat([tracks_master, tracks_ride, tracks_st],
+                               join='inner',
+                               sort=True)
+print(tracks_from_albums)
+
+# Concatenate the tables and add keys
+inv_jul_thr_sep = pd.concat([inv_jul, inv_aug, inv_sep], 
+                            keys=['7Jul','8Aug','9Sep'])
+
+# Group the invoices by the index keys and find avg of the total column
+avg_inv_by_month = inv_jul_thr_sep.groupby(level=0).agg({'total':'mean'})
+
+# Bar plot of avg_inv_by_month
+avg_inv_by_month.plot(kind='bar')
+plt.show()
+
+# Concatenate the classic tables vertically
+classic_18_19 =  pd.concat([classic_18, classic_19],
+                               ignore_index=True,)
+
+# Concatenate the pop tables vertically
+pop_18_19 = pd.concat([pop_18, pop_19],
+                               ignore_index=True,)
+
+# Merge classic_18_19 with pop_18_19
+classic_pop = classic_18_19.merge(pop_18_19, on='tid', how='inner')
+
+# Using .isin(), filter classic_18_19 rows where tid is in classic_pop
+popular_classic = classic_18_19[classic_18_19['tid'].isin(classic_pop['tid'])]
+
+# Print popular chart
+print(popular_classic)
+
+# Use merge_ordered() to merge gdp and sp500 on year and date
+gdp_sp500 = pd.merge_ordered(gdp, sp500, left_on='year', right_on='date', 
+                             how='left')
+
+# Print gdp_sp500
+print(gdp_sp500)
+# Use merge_ordered() to merge gdp and sp500, and forward fill missing values
+gdp_sp500 = pd.merge_ordered(gdp, sp500, left_on='year', right_on='date', 
+                             how='left',  fill_method='ffill')
+
+# Print gdp_sp500
+print (gdp_sp500)
+# Subset the gdp and returns columns
+gdp_returns = gdp_sp500[['gdp','returns']]
+
+# Print gdp_returns correlation
+print(gdp_returns.corr())
+# Use merge_ordered() to merge inflation, unemployment with inner join
+inflation_unemploy = pd.merge_ordered(inflation, unemployment, on='date', how='inner')
+
+# Print inflation_unemploy 
+print(inflation_unemploy)
+
+# Plot a scatter plot of unemployment_rate vs cpi of inflation_unemploy
+inflation_unemploy.plot(x='unemployment_rate', y='cpi', kind='scatter')
+plt.show()
+# Merge gdp and pop on date and country with fill and notice rows 2 and 3
+ctry_date = pd.merge_ordered(gdp, pop, on=['date', 'country'], 
+                             fill_method='ffill')
+
+# Print ctry_date
+print(ctry_date)
+# Merge gdp and pop on country and date with fill
+date_ctry = pd.merge_ordered(gdp, pop, on=['country', 'date'], fill_method='ffill')
+
+# Print date_ctry
+print(date_ctry)
