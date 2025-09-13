fb7dd186bbc1887e52bbb47dd0afd65019c4a68b
Zenderous314 <163520322+Zenderous314@users.noreply.github.com>
2025-09-13 17:38:20 +0100

    Associate Data Scientist/Introduction to Statistics Python/Random Numbers and Probability.py

---
+++ Associate Data Scientist/Introduction to Statistics Python/Random Numbers and Probability.py
@@ -0,0 +1,110 @@
+# Count the deals for each product
+counts = amir_deals['product'].value_counts()
+print(counts)
+# Count the deals for each product
+counts = amir_deals['product'].value_counts()
+
+# Calculate probability of picking a deal with each product
+probs = counts / amir_deals.shape[0]
+print(probs)
+# Set random seed
+np.random.seed(24)
+
+# Sample 5 deals without replacement
+sample_without_replacement = amir_deals.sample(5)
+print(sample_without_replacement)
+# Set random seed
+np.random.seed(24)
+
+# Sample 5 deals with replacement
+sample_with_replacement = amir_deals.sample(5, replace=True)
+print(sample_with_replacement)
+# Create a histogram of restaurant_groups and show plot
+restaurant_groups['group_size'].hist(bins=[2, 3, 4, 5, 6])
+plt.show()
+# Create probability distribution
+size_dist = restaurant_groups['group_size'].value_counts() / restaurant_groups.shape[0]
+
+# Reset index and rename columns
+size_dist = size_dist.reset_index()
+size_dist.columns = ['group_size', 'prob']
+
+print(size_dist)
+# Calculate expected value
+expected_value = np.sum(size_dist['group_size'] * size_dist['prob'])
+print(expected_value)
+# Subset groups of size 4 or more
+groups_4_or_more = size_dist[size_dist['group_size'] >= 4]
+
+# Sum the probabilities of groups_4_or_more
+prob_4_or_more = np.sum(groups_4_or_more['prob'])
+print(prob_4_or_more)
+# Min and max wait times for back-up that happens every 30 min
+min_time = 0
+max_time = 30
+# Import uniform from scipy.stats
+from scipy.stats import uniform
+
+# Calculate probability of waiting less than 5 mins
+prob_less_than_5 = uniform.cdf(5, min_time, max_time)
+print(prob_less_than_5)
+
+# Calculate probability of waiting more than 5 mins
+prob_greater_than_5 = 1 - uniform.cdf(5, min_time, max_time)
+print(prob_greater_than_5)
+
+# Calculate probability of waiting 10-20 mins
+prob_between_10_and_20 = uniform.cdf(20, min_time, max_time) - uniform.cdf(10, min_time, max_time)
+print(prob_between_10_and_20)
+
+# Set random seed to 334
+np.random.seed(334)
+# Generate 1000 wait times between 0 and 30 mins
+wait_times = uniform.rvs(0, 30, size=1000)
+
+# Create a histogram of simulated times and show plot
+plt.hist(wait_times)
+plt.show()
+
+# Import binom from scipy.stats
+from scipy.stats import binom
+
+# Set random seed to 10
+np.random.seed(10)
+
+# Simulate a single deal
+print(binom.rvs(1, 0.3, size=1))
+# Simulate 1 week of 3 deals
+print(binom.rvs(3, 0.3, size=1))
+
+
+# Simulate 52 weeks of 3 deals
+deals = binom.rvs(3, 0.3, size=52)
+
+# Print mean deals won per week
+print(np.mean(deals))
+
+# Probability of closing 3 out of 3 deals
+prob_3 = binom.pmf(3, 3, 0.3)
+
+print(prob_3)
+# Probability of closing <= 1 deal out of 3 deals
+prob_less_than_or_equal_1 = binom.cdf(1, 3, 0.3)
+
+print(prob_less_than_or_equal_1)
+# Probability of closing > 1 deal out of 3 deals
+prob_greater_than_1 = 1 - binom.cdf(1, 3, 0.3)
+
+print(prob_greater_than_1)
+
+# Expected number won with 30% win rate
+won_30pct = 3 * 0.3
+print(won_30pct)
+
+# Expected number won with 25% win rate
+won_25pct = 3 * 0.25
+print(won_25pct)
+
+# Expected number won with 35% win rate
+won_35pct = 3 * 0.35
+print(won_35pct)
