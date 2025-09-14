c97449a724a867dba1a84cec31d4112e76bdd0be
Zenderous <163520322+Zenderous314@users.noreply.github.com>
2025-09-14 10:41:05 +0100

    Associate Data Scientist/Introduction to Data Visualization with Matplotlib.py/Sharing visualizations with others.py

---
+++ Associate Data Scientist/Introduction to Data Visualization with Matplotlib.py/Sharing visualizations with others.py
@@ -0,0 +1,35 @@
+# Use the "ggplot" style and create new Figure/Axes
+plt.style.use("ggplot")
+fig, ax = plt.subplots()
+ax.plot(seattle_weather["MONTH"], seattle_weather["MLY-TAVG-NORMAL"])
+plt.show()
+# Use the "Solarize_Light2" style and create new Figure/Axes
+plt.style.use("Solarize_Light2")
+fig, ax = plt.subplots()
+ax.plot(austin_weather["MONTH"], austin_weather["MLY-TAVG-NORMAL"])
+plt.show()
+# Set figure dimensions and save as a PNG
+fig.set_size_inches([3, 5])
+fig.savefig("figure_3_5.png")
+# Extract the "Sport" column
+sports_column = summer_2016_medals["Sport"]
+
+# Find the unique values of the "Sport" column
+sports = sports_column.unique()
+
+# Print out the unique sports values
+print(sports)
+fig, ax = plt.subplots()
+
+# Loop over the different sports branches
+for sport in sports:
+  # Extract the rows only for this sport
+  sport_df = summer_2016_medals[summer_2016_medals["Sport"] == sport]
+  # Add a bar for the "Weight" mean with std y error bar
+  ax.bar(sport, sport_df["Weight"].mean(), yerr=sport_df["Weight"].std())
+
+ax.set_ylabel("Weight")
+ax.set_xticklabels(sports, rotation=90)
+
+# Save the figure to file
+fig.savefig("sports_weights.png")
