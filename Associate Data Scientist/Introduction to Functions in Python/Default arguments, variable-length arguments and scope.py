a2036e953cfcd805c65df1bf799a32273f668cd6
Zenderous <163520322+Zenderous314@users.noreply.github.com>
2025-09-15 12:57:20 +0100

    Associate Data Scientist/Introduction to Functions in Python/Default arguments, variable-length arguments and scope.py

---
+++ Associate Data Scientist/Introduction to Functions in Python/Default arguments, variable-length arguments and scope.py
@@ -0,0 +1,132 @@
+# Create a string: team
+team = "teen titans"
+
+# Define change_team()
+def change_team():
+    """Change the value of the global variable team."""
+
+    # Use team in global scope
+    global team
+
+    # Change the value of team in global: team
+    team = "justice league"
+
+# Print team
+print(team)
+
+# Call change_team()
+change_team()
+
+# Print team
+print(team)
+
+# Define three_shouts
+def three_shouts(word1, word2, word3):
+    """Returns a tuple of strings
+    concatenated with '!!!'."""
+
+    # Define inner
+    def inner(word):
+        """Returns a string concatenated with '!!!'."""
+        return word + '!!!'
+
+    # Return a tuple of strings
+    return (inner(word1), inner(word2), inner(word3))
+
+# Call three_shouts() and print
+print(three_shouts('a', 'b', 'c'))
+# Define echo
+def echo(n):
+    """Return the inner_echo function."""
+
+    # Define inner_echo
+    def inner_echo(word1):
+        """Concatenate n copies of word1."""
+        echo_word = word1 * n
+        return echo_word
+
+    # Return inner_echo
+    return inner_echo
+
+# Call echo: twice
+twice = echo(2)
+
+# Call echo: thrice
+thrice = echo(3)
+
+# Call twice() and thrice() then print
+print(twice('hello'), thrice('hello'))
+# Define shout_echo
+def shout_echo(word1, echo=1):
+    """Concatenate echo copies of word1 and three
+     exclamation marks at the end of the string."""
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1 * echo
+
+    # Concatenate '!!!' to echo_word: shout_word
+    shout_word = echo_word + '!!!'
+
+    # Return shout_word
+    return shout_word
+
+# Call shout_echo() with "Hey": no_echo
+no_echo = shout_echo('Hey')
+
+# Call shout_echo() with "Hey" and echo=5: with_echo
+with_echo = shout_echo('Hey', echo=5)
+
+# Print no_echo and with_echo
+print(no_echo)
+print(with_echo)
+# Define shout_echo
+def shout_echo(word1, echo=1, intense=False):
+    """Concatenate echo copies of word1 and three
+    exclamation marks at the end of the string."""
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1 * echo
+
+    # Make echo_word uppercase if intense is True
+    if intense is True:
+        # Make uppercase and concatenate '!!!': echo_word_new
+        echo_word_new = echo_word.upper() + '!!!'
+    else:
+        # Concatenate '!!!' to echo_word: echo_word_new
+        echo_word_new = echo_word + '!!!'
+
+    # Return echo_word_new
+    return echo_word_new
+
+# Call shout_echo() with "Hey", echo=5 and intense=True: with_big_echo
+with_big_echo = shout_echo('Hey', echo=5, intense=True)
+
+# Call shout_echo() with "Hey" and intense=True: big_no_echo
+big_no_echo = shout_echo('Hey', intense=True)
+
+# Print values
+print(with_big_echo)
+print(big_no_echo)
+# Define gibberish
+def gibberish(*args):
+    """Concatenate strings in *args together."""
+
+    # Initialize an empty string: hodgepodge
+    hodgepodge = ''
+
+    # Concatenate the strings in args
+    for word in args:
+        hodgepodge += word
+
+    # Return hodgepodge
+    return hodgepodge
+
+# Call gibberish() with one string: one_word
+one_word = gibberish('luke')
+
+# Call gibberish() with five strings: many_words
+many_words = gibberish("luke", "leia", "han", "obi", "darth")
+
+# Print one_word and many_words
+print(one_word)
+print(many_words)
