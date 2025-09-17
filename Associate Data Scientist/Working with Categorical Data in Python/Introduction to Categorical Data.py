ee7d4035da767ff6d3b8c4bb03a4b17269651c2e
Zenderous <163520322+Zenderous314@users.noreply.github.com>
2025-09-17 22:05:32 +0100

    Associate Data Scientist/Working with Categorical Data in Python/Introduction to Categorical Data.py

--- Associate Data Scientist/Working with Categorical Data in Python/Introduction to Categorical Data.py
+++ Associate Data Scientist/Working with Categorical Data in Python/Introduction to Categorical Data.py
@@ -27,3 +27,33 @@ print(medals)
 # Check the dtypes
 print(adult.dtypes)
 
+# Create a dictionary with column names as keys and "category" as values
+adult_dtypes = {
+   "Workclass": "category",
+   "Education": "category",
+   "Relationship": "category",
+   "Above/Below 50k": "category" 
+}
+# Read in the CSV using the dtypes parameter
+adult2 = pd.read_csv(
+  "adult.csv",
+  dtype=adult_dtypes
+)
+print(adult2.dtypes)
+
+# Group the adult dataset by "Sex" and "Above/Below 50k"
+gb = adult.groupby(by=['Sex', 'Above/Below 50k'])
+
+# Print out how many rows are in each created group
+print(gb.size())
+
+# Print out the mean of each group for all columns
+print(gb.mean())
+
+# Create a list of user-selected variables
+user_list = ["Education", "Above/Below 50k"]
+
+# Create a GroupBy object using this list
+gb = adult.groupby(by=user_list)
+# Find the mean for the variable "Hours/Week" for each group - Be efficient!
+print(gb["Hours/Week"].mean())
