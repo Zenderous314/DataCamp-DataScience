695ec9996b8b41e0014b218f9605155b629cdd47
Zenderous <163520322+Zenderous314@users.noreply.github.com>
2025-09-17 23:48:11 +0100

    Associate Data Scientist/Working with Categorical Data in Python/Categorical pandas Series.py

---
+++ Associate Data Scientist/Working with Categorical Data in Python/Categorical pandas Series.py
@@ -0,0 +1,57 @@
+# Check frequency counts while also printing the NaN count
+print(dogs['keep_in'].value_counts(dropna=False))
+
+# Switch to a categorical variable
+dogs["keep_in"] = dogs["keep_in"].astype("category")
+
+# Add new categories
+new_categories = ["Unknown History", "Open Yard (Countryside)"]
+dogs["keep_in"] = dogs["keep_in"].cat.add_categories(new_categories)
+
+# Check frequency counts one more time
+print(dogs["keep_in"].value_counts(dropna=False))
+
+# Set "maybe" to be "no"
+dogs.loc[dogs["likes_children"] == "maybe", "likes_children"] = "no"
+
+# Print out categories
+print(dogs["likes_children"].cat.categories)
+
+# Print the frequency table
+print(dogs["likes_children"].value_counts())
+
+# Remove the `"maybe" category
+dogs["likes_children"] = dogs["likes_children"].cat.remove_categories(["maybe"])
+print(dogs["likes_children"].value_counts())
+
+# Print the categories one more time
+print(dogs["likes_children"].cat.categories)
+
+# Create the my_changes dictionary
+my_changes = {
+    'Maybe?' : 'Maybe'
+}
+# Rename the categories listed in the my_changes dictionary
+dogs["likes_children"] = dogs["likes_children"].cat.rename_categories(my_changes)
+
+# Use a lambda function to convert all categories to uppercase using upper()
+dogs["likes_children"] =  dogs["likes_children"].cat.rename_categories(lambda c: c.upper())
+
+# Print the list of categories
+print(dogs["likes_children"].cat.categories)
+
+# Create the update_coats dictionary
+update_coats = {
+    'wirehaired': 'medium',
+    'medium-long': 'medium'
+}
+
+# Create a new column, coat_collapsed
+dogs["coat_collapsed"] = dogs["coat"].replace(update_coats)
+
+# Convert the column to categorical
+dogs["coat_collapsed"] = dogs["coat_collapsed"].astype("category")
+
+# Print the frequency table
+print(dogs["coat_collapsed"].value_counts(dropna=False))
+
