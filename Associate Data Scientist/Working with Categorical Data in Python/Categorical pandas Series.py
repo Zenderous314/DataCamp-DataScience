17b00fcadb71748181026d50ef750c843dcd5503
Zenderous <163520322+Zenderous314@users.noreply.github.com>
2025-09-21 08:43:08 +0100

    Associate Data Scientist/Working with Categorical Data in Python/Categorical pandas Series.py

--- Associate Data Scientist/Working with Categorical Data in Python/Categorical pandas Series.py
+++ Associate Data Scientist/Working with Categorical Data in Python/Categorical pandas Series.py
@@ -61,3 +61,52 @@ print(dogs["size"].cat.categories)
 # Reorder the categories using the list provided
 dogs["size"] = dogs["size"].cat.reorder_categories(new_categories=["small", "medium", "large"])
 
+# Reorder the categories, specifying the Series is ordinal, and overwriting the original series
+dogs["size"].cat.reorder_categories(
+  new_categories=["small", "medium", "large"],
+  ordered=True,
+  inplace=True
+)
+
+# Previous code
+dogs["size"].cat.reorder_categories(
+  new_categories=["small", "medium", "large"],
+  ordered=True,
+  inplace=True
+)
+
+# How many Male/Female dogs are available of each size?
+print(dogs.groupby('size')['sex'].value_counts())
+
+# Do larger dogs need more room to roam?
+print(dogs.groupby('size')['keep_in'].value_counts())
+
+# Fix the misspelled word
+replace_map = {"Malez": "male"}
+
+# Update the sex column using the created map
+dogs["sex"] = dogs["sex"].replace(replace_map)
+
+# Strip away leading whitespace
+dogs["sex"] = dogs["sex"].str.strip()
+
+# Make all responses lowercase
+dogs["sex"] = dogs["sex"].str.lower()
+
+# Convert to a categorical Series
+dogs["sex"] = dogs["sex"].astype("category")
+
+print(dogs["sex"].value_counts())
+
+# Print the category of the coat for ID 23807
+print(dogs.loc[23807, "coat"])
+
+# Find the count of male and female dogs who have a "long" coat
+print(dogs.loc[dogs["coat"] == "long", "sex"].value_counts())
+
+# Print the mean age of dogs with a breed of "English Cocker Spaniel"
+print(dogs.loc[dogs["breed"] == "English Cocker Spaniel", 'age'].mean())
+
+# Count the number of dogs that have "English" in their breed name
+print(dogs[dogs["breed"].str.contains('English', regex=False)].shape[0])
+
