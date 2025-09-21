4da74d68604797bb0b533bde93e313ab89f7c668
Zenderous <163520322+Zenderous314@users.noreply.github.com>
2025-09-22 00:38:51 +0100

    Associate Data Scientist/Introduction to Importing Data in Python/Working with relational databases in Python.py

--- Associate Data Scientist/Introduction to Importing Data in Python/Working with relational databases in Python.py
+++ Associate Data Scientist/Introduction to Importing Data in Python/Working with relational databases in Python.py
@@ -38,3 +38,42 @@ con.close()
 # Print head of DataFrame df
 print(df.head())
 
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute('SELECT LastName, Title FROM Employee')
+    df = pd.DataFrame(rs.fetchmany(size=3))
+    df.columns = rs.keys()
+
+# Print the length of the DataFrame df
+print(len(df))
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute('SELECT * FROM Employee WHERE EmployeeId >= 6')
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+
+# Print the head of the DataFrame df
+print(df.head())
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Open engine in context manager
+with engine.connect() as con:
+    rs = con.execute('SELECT * FROM Employee ORDER BY BirthDate')
+    df = pd.DataFrame(rs.fetchall())
+
+    # Set the DataFrame's column names
+    df.columns = rs.keys()
+
+# Print head of DataFrame
+print(df.head())
+
