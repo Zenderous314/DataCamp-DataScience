0aeb2f5c81ba7f67f11e37438c1075de3339c77e
Zenderous <163520322+Zenderous314@users.noreply.github.com>
2025-09-25 23:57:27 +0100

    Associate Data Scientist/Sampling in Python/Introduction to Sampling.py

--- Associate Data Scientist/Sampling in Python/Introduction to Sampling.py
+++ Associate Data Scientist/Sampling in Python/Introduction to Sampling.py
@@ -59,3 +59,10 @@ uniforms = np.random.uniform(low=-3, high=3, size=5000)
 # Plot a histogram of uniform values, binwidth 0.25
 plt.hist(uniforms, bins=np.arange(-3, 3.25, 0.25))
 plt.show()
+# Generate random numbers from a Normal(5, 2)
+normals = np.random.normal(loc=5, scale=2, size=5000)
+
+# Plot a histogram of normal values, binwidth 0.5
+plt.hist(normals, bins=np.arange(-2, 13.5, 0.5))
+plt.show()
+
