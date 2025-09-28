706983001ba689cb3c235f15b48fb00d1c19ae85
Zenderous <163520322+Zenderous314@users.noreply.github.com>
2025-09-28 15:16:59 +0100

    Associate Data Scientist/Machine Learning with Tree-Based Models in Python/Model Tuning.py

---
+++ Associate Data Scientist/Machine Learning with Tree-Based Models in Python/Boosting.py
@@ -0,0 +1,68 @@
+# Import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+# Import AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier
+
+# Instantiate dt
+dt = DecisionTreeClassifier(max_depth=2, random_state=1)
+
+# Instantiate ada
+ada = AdaBoostClassifier(base_estimator=dt, n_estimators=180, random_state=1)
+# Fit ada to the training set
+ada.fit(X_train, y_train)
+
+# Compute the probabilities of obtaining the positive class
+y_pred_proba = ada.predict_proba(X_test)[:, 1]
+# Import roc_auc_score
+from sklearn.metrics import roc_auc_score
+
+# Evaluate test-set roc_auc_score
+ada_roc_auc = roc_auc_score(y_test, y_pred_proba)
+
+# Print roc_auc_score
+print('ROC AUC score: {:.2f}'.format(ada_roc_auc))
+
+from sklearn.ensemble import GradientBoostingRegressor
+
+gb = GradientBoostingRegressor(max_depth=4, 
+                               n_estimators=200,
+                               random_state=2)
+
+# Import mean_squared_error as MSE
+from sklearn.metrics import mean_squared_error as MSE
+
+# Compute MSE
+mse_test = MSE(y_test, y_pred)
+
+# Compute RMSE
+rmse_test = mse_test**(1/2)
+
+# Print RMSE
+print('Test set RMSE of gb: {:.3f}'.format(rmse_test))
+# Import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
+# Instantiate sgbr
+sgbr = GradientBoostingRegressor(max_depth=4, 
+                                 subsample=0.9,
+                                 max_features=0.75,
+                                 n_estimators=200,
+                                 random_state=2)
+# Fit sgbr to the training set
+sgbr.fit(X_train, y_train)
+
+# Predict test set labels
+y_pred = sgbr.predict(X_test)
+
+# Import mean_squared_error as MSE
+from sklearn.metrics import mean_squared_error as MSE
+
+# Compute test set MSE
+mse_test = MSE(y_test, y_pred)
+
+# Compute test set RMSE
+rmse_test = mse_test**(1/2)
+
+# Print rmse_test
+print('Test set RMSE of sgbr: {:.3f}'.format(rmse_test))
